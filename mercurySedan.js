de776b356ca104cf5346c09c0e6d972ba40bba0c
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-03-22 19:19:18 -0700

    completed but console shows error

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,7 +13,32 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
 
+    }
+    start() {
+        if(this.fuel > 0){
+            console.log('Engine has started')
+            return this.started = true;
+        }
+    }
+    scheduleService(mileage) {
+        if(this.mileage > 30000){
+            console.log('Time for maintenance');
+            return this.scheduleService = true;
+        }
+    }
+}
 
 
 
